8710ff7ac206fc3cf5d695f88e899d78351f82cc
Alex Shapovalov <raketa.lovesmoke93@gmail.com>
2022-03-24 00:26:25 +0800

    First commit

---
+++ src/main/java/User.java
@@ -0,0 +1,35 @@
+public class User {
+    private String userName;
+    private int userAge;
+
+    public User(String userName, int userAge) {
+        this.userName = userName;
+        this.userAge = userAge;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.userName + "  " + "Age: " + this.userAge;
+    }
+
+    public static void main(String[] args) {
+        User user = new User("Alex", 28);
+        System.out.println(user.toString());
+    }
+}
